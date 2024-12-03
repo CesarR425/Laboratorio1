9b2cd1461bf9782847269c3e46ec260a4fe933fc
Cesar Rojas <ceromi.29@gmail.com>
2024-12-03 08:15:40 -0500

    completando funciones en javaScript

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
 console.log("Estoy en js")
+
+let $resultado = document.getElementById("resul")
+
+function fsumar() {
+    let num1 = parseInt(n1.value)
+    let num2 = parseInt(n2.value)
+    //console.log(typeof(num1))
+    $resultado.innerHTML = "La suma es:" + (num1+num2)
+}
+function frestar() {
+    let num1 = parseInt(n1.value)
+    let num2 = parseInt(n2.value)
+     $resultado.innerHTML ="La resta es:" + (num1-num2)
+}
+function fmultiplicar() {
+    let num1 = parseInt(n1.value)
+    let num2 = parseInt(n2.value)
+     $resultado.innerHTML="La multiplicacion es:" + (num1*num2)
+}
+function fdividir() {
+    let num1 = parseInt(n1.value)
+    let num2 = parseInt(n2.value)
+    if(num2===0)
+    { $resultado.innerHTML="La Division entre cero (0) no es posible"}
+    else{ $resultado.innerHTML="La divsion es:" + (num1/num2)}
+}
